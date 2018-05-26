a6ca1e57f399e2b6b1ecc9005dca1a0fd21c84cd
Paul <paul.derochebrune@epitech.eu>
2018-05-26 06:54:19 +0200

    use online api

--- src/modules/socket.js
+++ src/modules/socket.js
@@ -3,8 +3,8 @@ import { addMessage } from '../redux/actions/messages';
 import { fetchComments } from '../redux/actions/comments';
 import { fetchMessages } from '../redux/actions/messages';
 
-// const socket = new WebSocket('ws://epiblog-api.herokuapp.com');
-const socket = new WebSocket('ws://localhost:5000');
+const socket = new WebSocket('ws://epiblog-api.herokuapp.com');
+//const socket = new WebSocket('ws://localhost:5000');
 
 const sendEvent = (event) => {
   socket.send(JSON.stringify(event));
